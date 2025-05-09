6a3f437c7c49e6b77d4ba52e12b7defcc6d0e9ea
Bagusilmn <bagusilmanhuda@gmail.com>
2025-05-09 13:26:59 +0700

    `Update openai_api_key in chatbot_controller_popu.py`

--- controller/chat/chatbot_controller_popu.py
+++ controller/chat/chatbot_controller_popu.py
@@ -23,7 +23,7 @@ def load_chatbot_popu():
     llm = ChatOpenAI(
         model_name="google/gemini-2.0-flash-exp:free",
         # openai_api_key="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",
-        openai_api_key=" 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", #cadangan
+        openai_api_key="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", #cadangan
         openai_api_base="https://openrouter.ai/api/v1"
     )
 
