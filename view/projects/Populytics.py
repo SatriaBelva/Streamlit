674cca80ed1572e3eba259eba2e96acf991012ad
Bagusilmn <bagusilmanhuda@gmail.com>
2025-05-09 11:02:02 +0700

    "Update query string in Populytics.py to remove unnecessary word"

--- view/projects/Populytics.py
+++ view/projects/Populytics.py
@@ -38,7 +38,7 @@ with colText :
         st.title(f"kec. {selected_kecamatan} desa {selected_desa}")
         st.caption("Rekomendasi")
 
-        query = f"Berikan beberapa rekomendasi pilihan paket internet pada {selected_Product} di kecamatan {selected_kecamatan} desa {selected_desa} berdasarkan jumlah penduduk, pendidikan dan pekerjaan yang ada diwilayah tersebut, serta sesuai dengan tingkat ekonomi yang ada pada kecamatan {selected_kecamatan} dan berikan alasannya"
+        query = f"Berikan beberapa rekomendasi pilihan paket internet pada {selected_Product} di kecamatan {selected_kecamatan} desa {selected_desa} berdasarkan jumlah penduduk, pendidikan dan pekerjaan yang ada diwilayah tersebut, dan berikan alasannya"
         qa = load_chatbot_popu()
 
         if query:
