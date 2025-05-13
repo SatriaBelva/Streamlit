a81e7bedbd0ef79161d2f543bf49eb016b9ea4aa
Bagusilmn <bagusilmanhuda@gmail.com>
2025-05-13 14:53:35 +0700

    `Added CSS styles and container borders to ADOMobile.py`

--- view/projects/ADOMobile.py
+++ view/projects/ADOMobile.py
@@ -3,44 +3,63 @@ import pandas as pd
 from controller import *
 from model import *
 
-st.markdown(
-    """
-    <style>
-    section.main > div {background-color: #f0f2f6;}
-
-    div[data-testid="stVerticalBlock"] > div[style*="border"] {
-        background-color: white;
-        padding: 20px;
-        border-radius: 10px;
-        overflow: auto;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 col1, col2 = st.columns(2)
 
 with col1 :
     with st.container(border=True, height=700):
+        st.markdown(
+            """
+            <div style="background-color:white; padding:20px; border-radius:10px; border:1px solid #ddd;">
+            """,
+            unsafe_allow_html=True
+        )
+
         st.subheader("Jumlah Pelanggan Aktif")
         graphCB_Populasi()
 with col2 :
     with st.container(border=True, height=700):
+        st.markdown(
+            """
+            <div style="background-color:white; padding:20px; border-radius:10px; border:1px solid #ddd;">
+            """,
+            unsafe_allow_html=True
+        )
+
         st.subheader("Distribusi ARPU per Kecamatan")
         graph_Arpu()
 
 col3, col2 = st.columns(2)
 with col3 :
     with st.container(border=True, height=700):
+        
+        st.markdown(
+            """
+            <div style="background-color:white; padding:20px; border-radius:10px; border:1px solid #ddd;">
+            """,
+            unsafe_allow_html=True
+        )
+
         st.subheader("Jumlah Persebaran Menara BTS")
         graph_Site_pie()
 with col2 :
     with st.container(border=True, height=700):
+        st.markdown(
+            """
+            <div style="background-color:white; padding:20px; border-radius:10px; border:1px solid #ddd;">
+            """,
+            unsafe_allow_html=True
+        )
+
         st.subheader("Distribusi Outlet Mitra")
         graph_OUTLETPJP_pie()
 
 with st.container(border=True, height=700):
+    st.markdown(
+        """
+        <div style="background-color:white; padding:20px; border-radius:10px; border:1px solid #ddd;">
+        """,
+        unsafe_allow_html=True
+    )
     st.title("FB Share Reg & Youth per Kecamatan")
     graph_FBREG_FBYouth()
 
