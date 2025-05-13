c6d8020a6f84efed3e59f5cbde79c214eac09233
Bagusilmn <bagusilmanhuda@gmail.com>
2025-05-13 14:23:55 +0700

    `Refactor layout of ADOMobile.py view to use containers with borders and fixed heights`

--- view/projects/ADOMobile.py
+++ view/projects/ADOMobile.py
@@ -5,19 +5,23 @@ from model import *
 
 col1, col2 = st.columns(2)
 with col1 :
-    st.subheader("Jumlah Pelanggan Aktif")
-    graphCB_Populasi()
+    with st.container(border=True, height=600):
+        st.subheader("Jumlah Pelanggan Aktif")
+        graphCB_Populasi()
 with col2 :
-    st.subheader("Distribusi ARPU per Kecamatan")
-    graph_Arpu()
+    with st.container(border=True, height=600):
+        st.subheader("Distribusi ARPU per Kecamatan")
+        graph_Arpu()
 
 col3, col2 = st.columns(2)
 with col3 :
-    st.subheader("Jumlah Persebaran Menara BTS")
-    graph_Site_pie()
+    with st.container(border=True, height=600):
+        st.subheader("Jumlah Persebaran Menara BTS")
+        graph_Site_pie()
 with col2 :
-    st.subheader("Distribusi Outlet Mitra")
-    graph_OUTLETPJP_pie()
+    with st.container(border=True, height=600):
+        st.subheader("Distribusi Outlet Mitra")
+        graph_OUTLETPJP_pie()
 
 st.title("FB Share Reg & Youth per Kecamatan")
 graph_FBREG_FBYouth()
