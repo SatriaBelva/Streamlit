9cc457f200b4c1798e470b40f21e2aa687e2b145
Bagusilmn <bagusilmanhuda@gmail.com>
2025-05-13 14:45:42 +0700

    `Refactor ADOMobile.py: add CSS styles and adjust column heights`

--- view/projects/ADOMobile.py
+++ view/projects/ADOMobile.py
@@ -3,45 +3,46 @@ import pandas as pd
 from controller import *
 from model import *
 
-col1, col2 = st.columns(2)
-with col1:
-    with st.container():
-        # Tambahkan gaya CSS untuk background putih dan border
-        st.markdown(
-            """
-            <style>
-            div[data-testid="stVerticalBlock"] > div:nth-child(1) {
-                background-color: white;
-                padding: 20px;
-                border: 1px solid #ddd;
-                border-radius: 10px;
-                height: 600px;
-                overflow: auto;
-            }
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
+st.markdown(
+    """
+    <style>
+    section.main > div {background-color: #f0f2f6;}
 
+    div[data-testid="stVerticalBlock"] > div[style*="border"] {
+        background-color: white;
+        padding: 20px;
+        border-radius: 10px;
+        overflow: auto;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+col1, col2 = st.columns(2)
+
+with col1 :
+    with st.container(border=True, height=700):
         st.subheader("Jumlah Pelanggan Aktif")
         graphCB_Populasi()
 with col2 :
-    with st.container(border=True, height=600):
+    with st.container(border=True, height=700):
         st.subheader("Distribusi ARPU per Kecamatan")
         graph_Arpu()
 
 col3, col2 = st.columns(2)
 with col3 :
-    with st.container(border=True, height=600):
+    with st.container(border=True, height=700):
         st.subheader("Jumlah Persebaran Menara BTS")
         graph_Site_pie()
 with col2 :
-    with st.container(border=True, height=600):
+    with st.container(border=True, height=700):
         st.subheader("Distribusi Outlet Mitra")
         graph_OUTLETPJP_pie()
 
-st.title("FB Share Reg & Youth per Kecamatan")
-graph_FBREG_FBYouth()
+with st.container(border=True, height=700):
+    st.title("FB Share Reg & Youth per Kecamatan")
+    graph_FBREG_FBYouth()
 
 table()
 
