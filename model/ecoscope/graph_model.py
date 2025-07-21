da7322498c52a438522917f52a61a0b4880c2f1d
Bagusilmn <bagusilmanhuda@gmail.com>
2025-07-21 23:39:57 +0700

    `Fixed typo in SQL query: 'Daya_Beli_Kecamatan' -> 'Daya_Beli_Kecamatan' (case sensitivity)`

--- model/ecoscope/graph_model.py
+++ model/ecoscope/graph_model.py
@@ -40,23 +40,14 @@ def get_DayaBeli(kecamatan) :
         if kecamatan == "Semua" : 
             return conn.query('''
                 SELECT kecamatan.nama AS Kecamatan, 
-                SUM(kecamatan.daya_Beli_Kecamatan) AS "Daya Beli/ Kecamatan" 
+                SUM(kecamatan.Daya_Beli_Kecamatan) AS "Daya Beli/ Kecamatan" 
                 FROM kecamatan 
                 GROUP BY kecamatan.nama
             ''', ttl=600)
         elif kecamatan != "Semua" :
-            # return conn.query(f'''
-            #     SELECT 
-            #         kelurahan.nama AS Kelurahan, 
-            #         SUM(kelurahan.tidakbelum_sekolah + kelurahan.belum_tamatSD + kelurahan.tamatSD) AS "Tidak/putus sekolah, belum tamat SD, tamat SD"
-            #     FROM kelurahan
-            #     JOIN kecamatan ON kecamatan.KecamatanID = kelurahan.KecamatanID
-            #     WHERE kecamatan.nama = "{kecamatan}"
-            #     GROUP BY kelurahan.nama
-            #     ''', ttl=600)
             return conn.query(f'''
                 SELECT kecamatan.nama AS Kecamatan, 
-                SUM(kecamatan.daya_Beli_Kecamatan) AS "Daya Beli/ Kecamatan" 
+                SUM(kecamatan.Daya_Beli_Kecamatan) AS "Daya Beli/ Kecamatan" 
                 FROM kecamatan 
                 GROUP BY kecamatan.nama
                 ''', ttl=600)
